c93bd889f4989f77b5c339292c3796a76e544d29
Mike Halagan <sparkbox@ip-10-0-0-70.ec2.internal>
2017-05-23 20:18:50 +0000

    Added sort to scala script

--- extract-mhc.scala
+++ extract-mhc.scala
@@ -5,6 +5,7 @@ val bamfile = args(0)
 val outbam = args(1)
 val mhc = new ReferenceRegion("6", 28510120L, 33480577L)
 val alignments = sc.loadIndexedBam(bamfile, mhc)
-alignments.saveAsSam(outbam, asSingleFile = true)
+val alignmentsSorted = alignments.sortLexiocgraphically()
+alignmentsSorted.saveAsSam(outbam, asSingleFile = true)
 
 
