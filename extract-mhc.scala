5e9915570ed6c1b751710dd9cefcd5150bfbf67d
Mike Halagan <mhalagan@nmdp.org>
2017-06-08 13:54:02 -0500

    Fixed code to work with unmapped reads

--- extract-mhc.scala
+++ extract-mhc.scala
@@ -3,9 +3,10 @@ import org.bdgenomics.adam.models.ReferenceRegion
 
 val bamfile = args(0)
 val outbam = args(1)
-val mhc = new ReferenceRegion("6", 28510120L, 33480577L)
+val mhc = new ReferenceRegion("chr6", 28510120L, 33480577L)
 val alignments = sc.loadIndexedBam(bamfile, mhc)
-val alignmentsSorted = alignments.sortLexiocgraphically()
+val filteredAlignments = alignments.transform(rdd => {        rdd.filter(_.getReadMapped)      })
+val alignmentsSorted = filteredAlignments.sortLexicographically()
 alignmentsSorted.saveAsSam(outbam, asSingleFile = true)
 
 
